be558e3d0aede90b95e18f040c78e586bae602be
Francisco Soto <panchon.ues@gmail.com>
2024-04-04 18:16:12 -0700

    loginscreen

---
+++ src/config/theme/login-theme.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+
+
+export const loginStyle = StyleSheet.create({
+    flex1: {
+        flex: 1
+    },
+    touchBtn: {
+        marginBottom: 100, 
+        position:'absolute', 
+        bottom: 40, 
+        width: 200, 
+        marginHorizontal:'25%',
+        backgroundColor: 'white',
+        padding: 15,
+        borderRadius: 15
+    },
+    textBtn: {
+        fontWeight: '600', textAlign:'center'
+    }
+
+});
